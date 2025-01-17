63bad94aa784f6cbb3bba97ed76578745f6b0f7e
setoaryotomo <seto4137@gmail.com>
2025-01-17 19:40:13 +0700

    update tambahjadwal

--- pages/jadwalPeriksa/tambahJadwal.php
+++ pages/jadwalPeriksa/tambahJadwal.php
@@ -10,6 +10,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $jamMulai = $_POST["jamMulai"];
     $jamSelesai = $_POST["jamSelesai"];
 
+    $queryCheckSameDay = "SELECT * FROM jadwal_periksa WHERE id_dokter = '$idDokter' AND hari = '$hari'";
+    $resultCheckSameDay = mysqli_query($mysqli,$queryCheckSameDay);
+
+    if (mysqli_num_rows($resultCheckSameDay)>0) {
+        echo '<script>alert("Dokter sudah memiliki jadwal pada hari ini.");window.location.href="../../jadwalPeriksa.php";</script>';
+    } else {
+
     $queryOverlap = "SELECT * FROM jadwal_periksa INNER JOIN dokter ON jadwal_periksa.id_dokter = dokter.id INNER JOIN poli ON dokter.id_poli = poli.id WHERE id_poli = '$idPoli' AND hari = '$hari' AND ((jam_mulai < '$jamSelesai' AND jam_selesai > '$jamMulai') OR (jam_mulai < '$jamMulai' AND jam_selesai > '$jamMulai'))";
 
     $resultOverlap = mysqli_query($mysqli,$queryOverlap);
@@ -37,6 +44,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             // Jika terjadi kesalahan, tampilkan pesan error
             echo "Error: " . $query . "<br>" . mysqli_error($mysqli);
         }
+    }
     }
 }
 
